2e725a77de0c24a9661f01bd07ea3156a2227102
nescheytt <nahuel.scheytt@gmail.com>
2023-10-14 14:00:46 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['pbs.twimg.com'],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
