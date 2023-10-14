4e2f62978f1495e1f0188fa0e077a3853f4cbbc7
Nahuel Scheytt <nahuel.scheytt@gmail.com>
2023-10-14 15:21:55 +0100

    feat: banner + dark mode (#2)
    * feat: add body bg-background gradient
    
    * feat: create home + links
    
    * feat: add banner + dark mode
    
    * feat:remove dark

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,40 @@
 import Image from "next/image";
 import logoEventik from "@/public/logo-eventik.svg"
+import bannerTomatulugar from "@/public/banner-tomatulugar.jpg"
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <div className="flex items-center flex-col mx-auto w-full justify-center mt-8 px-8">
-      <div className="w-full flex items-center flex-col gap-16">
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
+      <div className="w-full flex items-center flex-col gap-24">
         <Image
           src={logoEventik}
           alt="Logo eventikapp"
           width={128}
           height={140}
+          priority
         />
 
-        <div className="w-full max-w-xl flex flex-col gap-4">
+        <div className="w-full flex flex-col items-center gap-4">
+          <Image
+            src={bannerTomatulugar}
+            alt="Banner tomatulugar"
+            width={745}
+            height={346}
+            className="mb-6"
+            priority
+          />
+
           <h2 className="text-center font-semibold text-xl">Elige tu inscripción según corresponda:</h2>
 
           <Link
             href="https://ar.eventik.app/e/conferencia-tomatulugar-2024-preparen-el-camino"
-            className={cn(buttonVariants({ variant: "secondary", size: "lg" }), "text-lg transition-all duration-300 ")}
+            className={
+              cn(buttonVariants({ size: "lg" }),
+                "w-full max-w-xl text-lg"
+              )}
             target="_blank"
           >
             🇦🇷 Argentinos
@@ -28,7 +42,10 @@ export default function Page() {
 
           <Link
             href="https://la.eventik.app/e/conferencia-tomatulugar-2024-preparen-el-camino?v=35357b9c8fe4"
-            className={cn(buttonVariants({ variant: "secondary", size: "lg" }), "text-lg transition-all duration-300")}
+            className={
+              cn(buttonVariants({ size: "lg" }),
+                "w-full max-w-xl text-lg"
+              )}
             target="_blank"
           >
             🌍 Internacionales
