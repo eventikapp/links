07b7497f9e08fd8ece929943f18c9cf1e5fa3b32
Nahuel Scheytt <nahuel.scheytt@gmail.com>
2023-10-14 18:23:27 +0100

    feat: dialog contact (#5)
    * feat: add dialog contact
    
    * feat: new max-w dialogs

--- app/emuba/page.tsx
+++ app/emuba/page.tsx
@@ -7,6 +7,9 @@ import logoEventik from "@/public/logo-eventik.svg"
 import bannerEmuba from "@/public/banner-aviva.jpg"
 
 import { buttonVariants } from "@/components/ui/button";
+import { DialogHeader } from '@/components/ui/dialog';
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+import { CreditCard, Ticket, Info } from 'lucide-react';
 
 export const metadata: Metadata = {
   title: `Eventik | EMUBA`
@@ -49,14 +52,57 @@ export default function Page() {
         </div>
 
         <div className="flex items-center gap-2">
-          <p>¿Necesitas ayuda?</p>
-          <Link
-            href="https://wa.me/5491122602647"
-            className="underline"
-            target="_blank"
-          >
-            Escríbenos
-          </Link>
+          <Dialog>
+            <DialogTrigger className={cn(buttonVariants({ variant: "secondary" }))}>Necesito ayuda</DialogTrigger>
+            <DialogContent className="max-md:max-w-full max-w-md max-md:h-full max-md:overflow-y-scroll max-md:max-h-screen max-md:flex max-md:flex-col max-md:justify-center max-md:gap-12">
+              <DialogHeader>
+                <DialogTitle>
+                  ¿Necesitas ayuda?
+                </DialogTitle>
+                <DialogDescription>
+                  Indicanos el  motivo principal para poder ayudarte
+                </DialogDescription>
+              </DialogHeader>
+
+              <div className="grid gap-1">
+                <Link href="https://wa.me/5491124058816" target="_blank">
+                  <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+                    <CreditCard className="mt-px h-5 w-5" />
+                    <div className="space-y-1">
+                      <p className="text-sm font-medium leading-none">Compra</p>
+                      <p className="text-sm text-muted-foreground">
+                        ¿Tuviste un problema al realizar la compra? ¿Hay un error en tu orden?
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+
+                <Link href="https://wa.me/5491124058816" target="_blank">
+                  <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+                    <Ticket className="mt-px h-5 w-5" />
+                    <div className="space-y-1">
+                      <p className="text-sm font-medium leading-none">Entrada</p>
+                      <p className="text-sm text-muted-foreground">
+                        ¿No recibiste tu entrada? ¿Quieres cambiar algún dato?
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+
+                <Link href="https://wa.me/5491122602647" target="_blank">
+                  <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+                    <Info className="mt-px h-5 w-5" />
+                    <div className="space-y-1">
+                      <p className="text-sm font-medium leading-none">Otras consultas</p>
+                      <p className="text-sm text-muted-foreground">
+                        Te contactaremos con el organizador.
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </div>
