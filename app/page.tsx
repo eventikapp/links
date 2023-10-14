34dd4f8f223528af66c30a3f700c76eb1562515c
Jonatan Flores <jonatanflores8@gmail.com>
2023-10-14 11:13:15 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
         <Image
           src={logoEventik}
           alt="Logo eventikapp"
-          width={140}
+          width={128}
           height={140}
         />
 
