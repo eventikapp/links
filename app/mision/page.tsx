0dba0ebdbc2a9077eef830fd34f3e7e25ef0c288
nescheytt <nahuel.scheytt@gmail.com>
2024-02-07 17:07:03 +0000

    update(mision): add w-full in grid for mobile

--- app/mision/page.tsx
+++ app/mision/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <main className="flex w-full flex-col items-center md:px-8">
       <section
-        className={cn("grid grid-cols-1 gap-6", {
+        className={cn("grid grid-cols-1 gap-6 max-md:w-full", {
           "grid-cols-1 md:grid-cols-2": quantityPublishEvents > 1,
         })}
       >
