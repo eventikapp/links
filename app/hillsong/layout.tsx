9bf5c0ddbc53f6ee4b5cec5973d434f25e84f082
Jonatan Flores <jonatanflores8@gmail.com>
2024-07-14 02:42:08 -0300

    Update layout.tsx

--- app/hillsong/layout.tsx
+++ app/hillsong/layout.tsx
@@ -1,11 +1,26 @@
 import type { Metadata } from "next"
 
 import Header from "@/components/pages/hillsong/header"
+import bannerSisterhood from "@/public/hillsong-sisterhood-conference.png"
 
 export const metadata: Metadata = {
   title: `Eventik | Hillsong`,
   icons: {
     icon: "/icon-eventik.png", // /public path
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://www.eventik.com/hillsong', // Reemplaza con la URL real de tu página
+    title: 'Sisterhood Conference',
+    description: 'Organizador: Hillsong',
+    images: [
+      {
+        url: bannerSisterhood, // Reemplaza con la ruta real de tu imagen Open Graph
+        width: 1200,
+        height: 630,
+        alt: 'Conferencia Sisterhood',
+      },
+    ],
   },
 }
 
