4977328ace28b1eb4bd2c5b149f4ab187ffbd72e
Jonatan Flores <jonatanflores8@gmail.com>
2024-07-14 02:18:58 -0300

    Update header.tsx

--- components/pages/hillsong/header.tsx
+++ components/pages/hillsong/header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
           </Link>
 
           <div className="absolute -top-4 right-4 md:-top-8 md:right-8 lg:right-0">
-            <DialogFAQ />
+            <DialogFAQ linkWhatsApp="https://wa.me/5491124058816" />
           </div>
         </nav>
 
