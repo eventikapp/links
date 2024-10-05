cd370eaecaffe2dac1569e990f62da8cf2872dc0
Jonatan Flores <jonatanflores8@gmail.com>
2024-10-05 20:32:30 -0300

    TTL 25 enabled

--- app/tomatulugar/page.tsx
+++ app/tomatulugar/page.tsx
@@ -73,7 +73,7 @@ const events: Event[] = [
       label: "🌍 Internacionales",
       url: "https://la.eventik.app/e/conferencia-tomatulugar-2025",
     },
-    publishStatus: "draft", // New field: 'published' or 'draft'
+    publishStatus: "published", // New field: 'published' or 'draft'
   },
 ]
 
