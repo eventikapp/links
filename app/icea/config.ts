373312057d3a025717d0e6ceb37d139d2eae4d48
Jonatan Flores <jonatanflores8@gmail.com>
2025-01-16 13:54:56 -0300

    new mision event: icea2025

---
+++ app/icea/config.ts
@@ -0,0 +1,43 @@
+import { OrganizerConfig } from "@/types/event"
+import logo from "@/public/logo-hillsong.jpg"
+import bannerSisterhood from "@/public/hillsong-casa-abierta-2025.png"
+
+export const ORGANIZER_CONFIG: OrganizerConfig = {
+  name: "MiSion CEM",
+  description: "Centro de Entrenamiento Ministerial",
+  logo: logo,
+  whatsAppLink: "",
+  socialLinks: [
+    { type: "globe", url: "https://misioninstituto.com/" },
+    {
+      type: "instagram",
+      url: "https://www.instagram.com/mision_instituto/",
+    },
+    { type: "video", url: "https://www.youtube.com/@MiSionCEM" },
+  ],
+  events: [
+    {
+      title: "INTIMIDAD CON EL AMADO 2025",
+      banner: bannerSisterhood,
+      dateTime: {
+        startDate: "2025-07-17",
+        startTime: "17:00",
+        endDate: "2025-07-19",
+        endTime: "22:00",
+      },
+      location: {
+        title: "Centro Costa Salguero",
+        subtitle: "Av. Costanera Rafael Obligado 1221, CABA",
+      },
+      primaryLink: {
+        label: "Residentes 🇦🇷 (ARS)",
+        url: "https://eventik.app/e/intimidad-con-el-amado-2025/",
+      },
+      secondaryLink: {
+        label: "Resto del 🌍 (USD)",
+        url: "https://mision-la.eventik.app/e/intimidad-con-el-amado-2025/",
+      },
+      publishStatus: "published",
+    },
+  ],
+}
