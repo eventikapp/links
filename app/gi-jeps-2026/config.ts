68ba338a8aeeb38939ca0fc3ca75ee941c7e4012
Jonatan Flores <jonatanflores8@gmail.com>
2025-02-27 16:22:58 -0300

    new eventp ms

---
+++ app/gi-jeps-2026/config.ts
@@ -0,0 +1,43 @@
+import { OrganizerConfig } from "@/types/event"
+import organizerLogo from "@/public/logo-mision.svg"
+import eventCover from "@/public/gi-jeps-2026.jpg"
+
+export const ORGANIZER_CONFIG: OrganizerConfig = {
+  name: "MiSion CEM",
+  description: "Centro de Entrenamiento Ministerial",
+  logo: organizerLogo,
+  whatsAppLink: "",
+  socialLinks: [
+    { type: "globe", url: "https://misioninstituto.com/" },
+    {
+      type: "instagram",
+      url: "https://www.instagram.com/mision_instituto/",
+    },
+    { type: "video", url: "https://www.youtube.com/@MiSionCEM" },
+  ],
+  events: [
+    {
+      title: "GI-JEPS 2026",
+      banner: eventCover,
+      dateTime: {
+        startDate: "2026-02-23",
+        startTime: "17:00",
+        endDate: "2026-02-28",
+        endTime: "22:00",
+      },
+      location: {
+        title: "Iglesia El Encuentro",
+        subtitle: "Arana 1372, Luis Guillón, Buenos Aires.",
+      },
+      primaryLink: {
+        label: "Residentes 🇦🇷 (ARS)",
+        url: "https://eventik.app/gi-jeps-2026/",
+      },
+      secondaryLink: {
+        label: "Resto del 🌎 (USD)",
+        url: "https://mision-la.eventik.app/gi-jeps-2026/",
+      },
+      publishStatus: "published",
+    },
+  ],
+}
