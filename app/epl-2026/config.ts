2a9f2f133d759c9607ffb9f573fd8e1f72d9ca83
eventik <jonatanflores8@gmail.com>
2025-08-07 17:35:07 -0300

    Add EPL 2026 event page and assets

---
+++ app/epl-2026/config.ts
@@ -0,0 +1,43 @@
+import { OrganizerConfig } from "@/types/event"
+import logo from "@/public/logo-ccvida.png"
+import bannerSisterhood from "@/public/banner-epl-2026.jpg"
+
+export const ORGANIZER_CONFIG: OrganizerConfig = {
+  name: "Centro Cristiano Vida",
+  description: "¡La iglesia sos vos!x§x§",
+  logo: logo,
+  whatsAppLink: "",
+  socialLinks: [
+    { type: "globe", url: "https://ccvidaargentina.org/site/" },
+    {
+      type: "instagram",
+      url: "https://www.instagram.com/ccvidaargentina",
+    },
+    { type: "video", url: "https://www.youtube.com/user/buscamosmas" },
+  ],
+  events: [
+    {
+      title: "EPL 2026 - Escuela de Pastores y Lideres",
+      banner: bannerSisterhood,
+      dateTime: {
+        startDate: "2026-02-02",
+        startTime: "19:00",
+        endDate: "2026-02-05",
+        endTime: "21:00",
+      },
+      location: {
+        title: "Centro Cristiano Vida",
+        subtitle: "Don Bosco 999, San Isidro, Buenos Aires, Argentina.",
+      },
+      primaryLink: {
+        label: "Residentes 🇦🇷 (ARS)",
+        url: "https://ccvida.fint.app/eventos/epl-2026-arg",
+      },
+      secondaryLink: {
+        label: "Resto del 🌍 (USD)",
+        url: "https://ccvida.fint.app/eventos/epl-2026-ext",
+      },
+      publishStatus: "published",
+    },
+  ],
+}
